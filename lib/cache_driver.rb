2ba93d883eaeed4f63b5d5ef2d2e917939cb6f62
goshan <han.qiu@dena.jp>
2016-01-31 00:37:45 +0900

    added inspect rake task

--- lib/cache_driver.rb
+++ lib/cache_driver.rb
@@ -4,6 +4,15 @@ require "cache_driver/cache_record"
 require "cache_driver/cache_util"
 require "cache_driver/file_cache_util"
 
+
+module CacheDriver
+	class Railtie < ::Rails::Railtie
+		rake_tasks do 
+			load 'tasks/cache.rake'
+		end
+	end
+end
+
 unless CacheDriver.configed?
 	CacheDriver.setup do |config|
 		config.store = :file
